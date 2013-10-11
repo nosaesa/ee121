b5915328a1c5d8f2796aec571666bdf02fda7172
nosaesa <jtlievense@gmail.com>
2013-10-11 14:49:50 -0700

    This shit is dope
    Multitone frequencies shit is working

--- FSK/makeCodebookMulti.m
+++ FSK/makeCodebookMulti.m
@@ -3,6 +3,6 @@ function C = makeCodebookMulti(k,fc,fs,D,bitNum)
     t = 0:1/fs:T-1/fs;
     C = zeros(2^k,length(t));
     for i = 1:2^k
-        C(i,:) = cos(2*pi*i*(fc+bitNum*10)*t);
+        C(i,:) = cos(2*pi*i*(fc+bitNum*30)*t);
     end
 end
