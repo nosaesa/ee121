dea0e3a0294df584b99c30dd547264984f78f64a
Tim Brown <tbrown126@dhcp-46-109.eecs.berkeley.edu>
2013-10-12 17:18:21 -0700

    initial code for generator matrix and packet generation

--- realProject1/codePacketGenerator.m
+++ realProject1/codePacketGenerator.m
@@ -1,3 +1,9 @@
-function [packets] = codePacketGenerator(G, bits)
-
+function [packets] = codePacketGenerator(G, bits, fieldSize)
+    chunkSize = size(G, 2);
+    packets = [];
+    for i = 0:length(bits)/chunkSize-1
+        start = i*chunkSize + 1;
+        packets = [packets; G*bits(start:start+chunkSize-1)];
+    end
+    packets = mod(packets, 
 end
--- realProject1/createGeneratorMatrix.m
+++ realProject1/createGeneratorMatrix.m
@@ -1,3 +1,5 @@
 function [G] = createGeneratorMatrix(degree, chunkSize, rate)
-
+    rows = chunkSize/rate;
+    columns = chunkSize;
+    G = ceil(rand(rows, columns)*degree);
 end
