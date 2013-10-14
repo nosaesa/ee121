667cc4badd84dd236c014d8c37412a11b285d014
Rishi Sharma <sharma409@gmail.com>
2013-10-13 22:35:24 -0700

    pushing synchronize so far

--- realProject1/synchronize.m
+++ realProject1/synchronize.m
@@ -1,7 +1,10 @@
-function [A] = synchronize(rcv,packlength,fs)
+function [A] = synchronize(rcv,packLength,fs,fc,P)
 %SYNCHRONIZE Find chirps and get data streams from rcv
-%   Detailed explanation goes here
+
 t = 0:1/fs:0.02;
+timeStep = 0:1/fs:P/fc-1/fs;
+packLengthSamples = (packLength)*length(timeStep) + length(t);
+% packLengthSamples is the number of samples in a packet (with pulse)
 SSpulse = chirp(t,600,0.02,2000,'linear');
 syncPulse = chirp(t,400,0.02,8000,'linear');
 % Get pulse to start and stop data transmission%
@@ -10,17 +13,34 @@ startStop = fftshift(abs(startStop));
 startStop = startStop(1:length(startStop)/2);
 [~,sortIndex] = sort(startStop(:),'descend');
 maxIndex = [sortIndex(1) 0];
-epsilon = 200
+epsilon = 200;
 for i = sortIndex(2:end)
     if abs(i - maxIndex(1)) > epsilon
         maxIndex(2) = i;
         break
     end
 end
-startSample = min(maxIndex) + length(t) + 1;
+startSample = min(maxIndex);
 endSample = max(maxIndex) - 1;
 %-----------------------------------------------
+% Find the actual peaks in the signal and create expected packets
+messageLength = endSample - startSample;
+numPeaks = round(messageLength/packLengthSamples); %floor or round
+i = 0:numPeaks-1;
+syncorr = xcorr(rcv,syncPulse);
+syncorr = fftshift(abs(syncorr));
+syncorr = syncorr(1:length(syncorr)/2);
+syncPeaks = [startSample zeros(1, numpeaks - 1)]
+[~,sortIndex] = sort(syncorr(:),'descend');
+peaks = 1;
+for i = sortIndex 
+    if max(abs(i - find(syncPeaks)) > epsilon
+        
+        peaks = peaks + 1
+        if peaks >= numPeaks
+            break
+        end
+    end
+end
 
-
-ends = [];
 end
--- realProject1/transmit.m
+++ realProject1/transmit.m
@@ -3,7 +3,7 @@ function transmit(Z,fs)
 Z = Z';
 t = 0:1/fs:0.02;
 pulse = chirp(t,600,0.02,2000,'linear');
-% sound([pulse Z(length(t)+1:end) pulse] ,fs);
+sound([pulse Z(length(t)+1:end) pulse] ,fs);
 
 %-----TEST CODE JUNK THROW AWAY IF NOT IN USE
 %{
