269b69ff9e36f164e7dcbe33b9158d9169061cf9
Rishi Sharma <sharma409@gmail.com>
2013-10-16 20:11:00 -0700

    WE STILL NEED TO FIX EMPTY SLOTS......ALSO EDGECASE TRANSMIT(1)

--- sliceFileAddHeader.m
+++ sliceFileAddHeader.m
@@ -1,19 +1,25 @@
-function [files] = sliceFileAddHeader(bits,headerLength,numChunks)
+function [files] = sliceFileAddHeader(bits,headerLength,numFiles,fileName)
 %SLICEFILEADDHEADER Slices file into chunks and add header packet to each
-chunkLength = floor(length(bits)/(numChunks-1));
-padBits = chunkLength - mod(length(bits),numChunks-1);
+chunkLength = floor(length(bits)/(numFiles-1));
+padBits = chunkLength - mod(length(bits),numFiles-1);
 
-chunkHeaders = zeros(numChunks,headerLength);
-for i = 1:numChunks
-if i == numChunks
-    chunkHeaders(i,:) = makeheader(bits((i-1)*chunkLength+1:end));
+Headers = zeros(numFiles,headerLength);
+for i = 1:numFiles
+if i == numFiles
+    Headers(i,:) = makeHeader(fileName,(bits((i-1)*chunkLength+1:end))',mod(i-1,4));
 else
-    chunkHeaders(i,:) = makeHeader(bits((i-1)*chunkLength+1:i*chunkLength));
+    Headers(i,:) = makeHeader(fileName,(bits((i-1)*chunkLength+1:i*chunkLength))',mod(i-1,4));
 end
 end
 
+%padding for file length
 paddedBits = [bits zeros(1,padBits)];
-files = [chunkHeaders reshape(paddedBits,numChunks,chunkLength)];
+files = [Headers reshape(paddedBits,numFiles,chunkLength)];
 
+%padding for hamming
+fileLength = length(files(1,:));
+hammPad = zeros(numFiles,(floor(fileLength/11)+1)*11 - fileLength);
+files = [files hammPad];
+keyboard;
 end
 
